18b122c5caf02b6f9c9b67153ba9da66cee5c9fa
Fidan <aslanovafidan299@gmail.com>
2025-06-14 16:47:49 +0000

    first commit

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script concatenates two strings and prints the result
 str1 = "Welcome to "
 str2 = "Holberton School!"
 print(str1 + str2)
+# This script concatenates str1 and str2
