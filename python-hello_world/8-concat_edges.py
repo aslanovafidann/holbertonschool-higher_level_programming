0ffe9e7fe59cb2baeba1549d9f01495aba9859ca
Fidan <aslanovafidan299@gmail.com>
2025-06-14 18:23:36 +0000

    first commit

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming with Python"
-
-print(f"{str1} {str2}")
+str = "Holberton School"
+print(str[0:9] + str[9:13] + str[15:19] + str[13] + str[14] + str[19:])
