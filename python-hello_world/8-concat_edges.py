bca4148a1127bc5ec082d97a6e5ccb1cd47c94a9
Fidan <aslanovafidan299@gmail.com>
2025-06-14 18:25:25 +0000

    first commit

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Holberton School"
-print(str[0:9] + str[9:13] + str[15:19] + str[13] + str[14] + str[19:])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[0:6])
