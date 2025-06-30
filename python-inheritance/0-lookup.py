7e58325801a043aa978f4545a2065768ea6de5ca
Fidan <aslanovafidan299@gmail.com>
2025-06-30 17:55:27 +0000

    My Commit

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module to return list of available attributes and methods of an object."""
+"""Module that defines the lookup function"""
+
 
 def lookup(obj):
-    """Return list of attributes and methods of obj."""
+    """Return a list of available attributes and methods of an object"""
     return dir(obj)
