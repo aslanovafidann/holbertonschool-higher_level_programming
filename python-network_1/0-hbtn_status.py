973405dff656ef0e7d1fd8ad1f3ce44d52e71fe8
Fidan <aslanovafidan299@gmail.com>
2025-07-12 19:50:26 +0000

    My commit

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using urllib and displays
+the response body type, content (bytes), and decoded utf-8 content.
+"""
+
+import urllib.request
+
+def main():
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+if __name__ == "__main__":
+    main()
