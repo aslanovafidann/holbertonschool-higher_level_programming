757ca9cf69d50f2ee5b66ecd27afd50f2f9c022e
Fidan <aslanovafidan299@gmail.com>
2025-07-12 20:07:22 +0000

    My commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,15 +6,16 @@ Prints the response body type and content as string.
 
 import requests
 
-
 def main():
     url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
+    headers = {
+        "cfclearance": "true",
+        "User-Agent": "Mozilla/5.0"
+    }
+    response = requests.get(url, headers=headers)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
 
-
 if __name__ == "__main__":
     main()
-
