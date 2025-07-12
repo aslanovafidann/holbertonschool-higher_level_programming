92764352ae3a9a5be4711d3c7bfabd3f290937f4
Fidan <aslanovafidan299@gmail.com>
2025-07-12 19:51:56 +0000

    My commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,13 +2,18 @@
 """
 Fetches https://intranet.hbtn.io/status using urllib and displays
 the response body type, content (bytes), and decoded utf-8 content.
+Adds header 'cfclearance': 'true' to bypass firewall.
 """
 
 import urllib.request
 
 def main():
     url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    headers = {
+        "cfclearance": "true"
+    }
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as response:
         body = response.read()
 
         print("Body response:")
