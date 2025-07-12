a5f6d7f74d00fe5590aad67e4ac19de7130dcbc7
Fidan <aslanovafidan299@gmail.com>
2025-07-12 20:08:58 +0000

    My commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,7 +4,9 @@ Fetches https://intranet.hbtn.io/status using requests.
 Prints the response body type and content as string.
 """
 
+
 import requests
+
 
 def main():
     url = "https://intranet.hbtn.io/status"
@@ -16,6 +18,8 @@ def main():
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
+
+
 
 if __name__ == "__main__":
     main()
